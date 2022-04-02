38d6d502103cfa2db650c68531956651b13fcce6
Victor Zhang <vwzhang@kc.rr.com>
2022-04-02 02:29:53 -0500

    Add dbcontext

---
+++ WebApiBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+
+namespace WebApiBackend.Data
+{
+    using Duende.IdentityServer.EntityFramework.Options;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Options;
+    using Models;
+
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
