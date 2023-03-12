8a583b7205d0a96d5a332b19a534853dd3960c65
Victor Zhang <vwzhang@kc.rr.com>
2023-03-11 22:07:25 -0600

    Correct Dev branch

---
+++ MinimalWebApi/Models/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalWebApi.Models
+{
+    public class DbInitializer
+    {
+        private readonly TodoDb _context;
+
+        public DbInitializer(TodoDb context)
+        {
+            _context = context;
+        }
+
+        public void Run()
+        {
+            _context.Database.EnsureCreated();
+            _context.Todos.Add(new Todo { Name = "Walk the dog", IsComplete = false });
+            _context.Todos.Add(new Todo { Name = "Clean the room", IsComplete = true });
+            _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ MinimalWebApi/Models/TodoDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalWebApi.Models
+{
+    public class TodoDb: DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+        {
+            //Database.SetInitializer<TodoDb>(new CreateDatabaseIfNotExists<TodoDb>());
+        }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+
+    }
+}
